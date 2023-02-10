a840ce582131786b4b5cb68bb70d54dc0f4dce77
Vinicius Bento <vinicius_bento10@hotmail.com>
2023-02-09 21:52:36 -0300

    ex

---
+++ aula11/ex008.js
@@ -0,0 +1,9 @@
+ var vel= 60
+
+ console.log(`A velocidade do seu carro é ${vel}Km/h`)
+ 
+ if(vel > 60) {
+    console.log('Você escedeu a velocidade maxima permitida e será MULTADO!')
+ }else(vel<60) 
+    console.log('Voce esta na velocidade permitida, prossiga!')
+ 
